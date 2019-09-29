1a2e9180eb883391b18cdc501be8e04b9e6bdaa2
Mani <maani.cr7@gmail.com>
2019-09-29 20:30:50 +1000

    Updates

---
+++ precache-manifest.e3f4a69275de594604883cc7ebe5d4d3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd6142c86fa690c0950e4e04075e47eb",
+    "url": "/anotherboringtodoapp/index.html"
+  },
+  {
+    "revision": "db074495d3e461e8baef",
+    "url": "/anotherboringtodoapp/static/css/main.757b1877.chunk.css"
+  },
+  {
+    "revision": "a522b193fef0da45fc54",
+    "url": "/anotherboringtodoapp/static/js/2.71373447.chunk.js"
+  },
+  {
+    "revision": "db074495d3e461e8baef",
+    "url": "/anotherboringtodoapp/static/js/main.73adf9a1.chunk.js"
+  },
+  {
+    "revision": "bbc8b71f24519a538fe2",
+    "url": "/anotherboringtodoapp/static/js/runtime-main.2bc37fae.js"
+  }
+]);
